fe24c07d6cd96a86b36cffb341237a1e1163bd5d
dorachan1029 <7115497+hmaruyama@users.noreply.github.com>
2018-08-25 18:39:09 +0900

    Use messaging API

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-var http = require('http')
+'use strict';
 
-http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/plain" })
-    response.end("Hello World\n")
-}).listen(process.env.PORT)
+const express = require('express');
+const line = require('@line/bot-sdk');
+const PORT = process.env.PORT || 3000;
+
+const config = {
+    channelSecret: process.env.CHANNEL_ACCESS_TOKEN,
+    channelAccessToken: process.env.CHANNEL_SECRET
+};
+
+const app = express();
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+    console.log(req.body.events);
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text //実際に返信の言葉を入れる箇所
+  });
+}
+
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
