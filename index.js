89aec93efc35abd498cc7c05760f0ba286ded03c
dorachan1029 <7115497+hmaruyama@users.noreply.github.com>
2018-08-25 20:45:22 +0900

    Change getting question list

--- index.js
+++ index.js
@@ -20,25 +20,59 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 const client = new line.Client(config);
 
+function shuffle(array) {
+  var n = array.length, t, i;
+
+  while (n) {
+    i = Math.floor(Math.random() * n--);
+    t = array[n];
+    array[n] = array[i];
+    array[i] = t;
+  }
+  return array;
+}
+
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
-  let qList = ['好きな食べ物は何ですか？', '出身地はどこですか？', '好きな本は何ですか？', '趣味は何ですか？'];
-
-  return client.replyMessage(event.replyToken,
-    [
-      {
-        type: 'text',
-        text: qList[Math.floor(Math.random() * qList.length)]
-      },
-      {
-        type: 'text',
-        text: qList[Math.floor(Math.random() * qList.length)]
-      }
-    ]
-);
+  let qList = [
+    {
+      type: 'text',
+      text: '好きな食べ物はなんですか？'
+    },
+    {
+      type: 'text',
+      text: '出身地はどこですか？'
+    },
+    {
+      type: 'text',
+      text: '趣味はなんですか？'
+    },
+    {
+      type: 'text',
+      text: '好きな本はなんですか？'
+    },
+    {
+      type: 'text',
+      text: '好きなアーティストはいますか？'
+    },
+    {
+      type: 'text',
+      text: '休日は何をして過ごしていますか？'
+    },
+    {
+      type: 'text',
+      text: 'アウトドア派ですか、インドア派ですか？'
+    },
+    {
+      type: 'text',
+      text: 'どこに住んでいるんですか？'
+    }
+  ];
+  let randomQList = shuffle(qList).slice(0, 4);
+  return client.replyMessage(event.replyToken,randomQList;
 }
 
 app.listen(PORT);
