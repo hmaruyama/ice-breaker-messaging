22f6e1d69391374363fa8b1265df5f5cd7e1a492
dorachan1029 <7115497+hmaruyama@users.noreply.github.com>
2018-08-26 12:10:13 +0900

    Test

--- index.js
+++ index.js
@@ -87,16 +87,21 @@ function handleEvent(event) {
   }
 
 
-
-  const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
-  async () => {
-    await asyncForEach([1, 2, 3], async (num) => {
-      await waitFor(1000)
-      console.log(num)
-    })
-    console.log('Done')
+  async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+      await callback(array[index], index, array)
+    }
   }
-
+  const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+  const start = async () => {
+    await asyncForEach(randomQList, async (num) => {
+      await waitFor(1000);
+      client.pushMessage(userId, item);
+      console.log(num);
+    })
+    console.log('Done');
+  }
+  start();
 
 
 
